f48cbcb77e2ceb637ab3f5812bba0bc6a53eb9a5
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2023-01-18 21:45:07 +0100

    ___

--- edit.php
+++ edit.php
@@ -14,7 +14,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="./img/profile_pic.jpg" type="image/png">
   <link href="css/style.css" rel="stylesheet">
-  <!-- <link rel="stylesheet" href="./css/framework.css"> -->
   <link rel="stylesheet" href="./css/edit.css">
   <title>Edit Account</title>
 </head>
